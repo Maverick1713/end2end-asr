c003080092ef67a57faca8a24a029a954a54eda2
Maverick1713 <tpratyushsingh1713@gmail.com>
2025-04-03 09:45:45 +0530

    Update common_layers.py
    changes made to common_layers

--- models/common_layers.py
+++ models/common_layers.py
@@ -216,7 +216,8 @@ class ScaledDotProductAttention(nn.Module):
         attn = attn / self.temperature
 
         if mask is not None:
-            attn = attn.masked_fill(mask, -np.inf)
+            attn = attn.masked_fill(mask.bool(), -np.inf)
+
 
         attn = self.softmax(attn)
         attn = self.dropout(attn)
